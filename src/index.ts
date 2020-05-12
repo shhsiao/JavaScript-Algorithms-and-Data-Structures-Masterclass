40176ffb92d1857234bdd7f59bc88ef4e18deded
Sam <shenghanhsiao@gmail.com>
2020-05-12 15:25:22 +0800

    add binary-search

--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as http from 'http';
-
-const app = http.createServer((req, res) => {
-    console.log(req.url);
-    res.end('hello,world');
-});
-console.log('start listening...');
-app.listen(3000);
---
+++ src/search/binary-search.ts
@@ -0,0 +1,40 @@
+function binarySearchLoopVersion(sortedList: number[], target: number): number {
+    let left = 0,
+        right = sortedList.length - 1,
+        middle = (right + left) >> 1;
+    while (left <= right) {
+        if (sortedList[middle] === target) {
+            return middle;
+        }
+        if (sortedList[middle] > target) {
+            right = middle - 1;
+        } else {
+            left = middle + 1;
+        }
+        middle = (right + left) >> 1;
+    }
+    return -1;
+}
+
+function binarySearchRecusionVersion(
+    sortedList: number[],
+    target: number
+): boolean {
+    let left = 0,
+        right = sortedList.length - 1,
+        middle = (right + left) >> 1;
+    if (sortedList[middle] === target) {
+        return true;
+    }
+    if (sortedList.length === 1) {
+        return false;
+    }
+    if (sortedList[middle] > target) {
+        return binarySearchRecusionVersion(sortedList.slice(0, middle), target);
+    } else {
+        return binarySearchRecusionVersion(
+            sortedList.slice(middle + 1),
+            target
+        );
+    }
+}
